45a117f65a28e598d1325666f5569d4691764d8c
ArthurAcauan <142455959+ArthurAcauan@users.noreply.github.com>
2023-08-22 21:45:54 -0300

    Create exercicio.js

---
+++ exercicio.js
@@ -0,0 +1,48 @@
+/*
+* Arthur Felipe Acauan
+*
+*/
+
+//exercicio1
+//resposta=10, 10 5
+
+let a = 10
+let b = 10
+
+console.log(b);
+b = 5
+console.log(a, b);
+
+//exercicio2
+//resposta=10 10 10
+
+let A = 10
+let B = 20
+C = A
+B = C
+A = B
+console.log(A, B, C);
+
+//exercicio3
+//resposta=let horasTrabalhadas = prompt("Quantas horas voce...");
+//let valorDia = prompt("quanto voce recebe por...");
+
+//exercicio escrita de codigo
+//1a
+let nome;
+//1b
+let idade;
+//1c
+console.log(typeof nome);
+console.log(typeof idade);
+//1d=criamos uma variavel indefinida, mas que pode se atribuir um valor.
+//1e
+nome = prompt("Qual é o seu nome?");
+idade = prompt("Qual a sua idade?");
+//1f
+console.log(typeof nome);
+console.log(typeof idade);
+//1g
+console.log("Olá", nome, "você tem", idade, "anos");
+
+//2a
