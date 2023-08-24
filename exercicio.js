b45358b34d75643603c5a8510ddab75b016c09b3
ArthurAcauan <142455959+ArthurAcauan@users.noreply.github.com>
2023-08-24 20:11:10 -0300

    Update exercicio.js

--- exercicio.js
+++ exercicio.js
@@ -6,22 +6,22 @@
 //exercicio1
 //resposta=10, 10 5
 
-let a = 10
-let b = 10
+//let a = 10
+//let b = 10
 
-console.log(b);
-b = 5
-console.log(a, b);
+//console.log(b);
+//b = 5
+//console.log(a, b);
 
 //exercicio2
 //resposta=10 10 10
 
-let A = 10
-let B = 20
-C = A
-B = C
-A = B
-console.log(A, B, C);
+//let A = 10
+//let B = 20
+//C = A
+//B = C
+//A = B
+//console.log(A, B, C);
 
 //exercicio3
 //resposta=let horasTrabalhadas = prompt("Quantas horas voce...");
@@ -46,3 +46,31 @@ console.log(typeof idade);
 console.log("Olá", nome, "você tem", idade, "anos");
 
 //2a
+prompt("Você está usando uma roupa azul hoje?");
+prompt("você tem mais de 20 anos?");
+prompt("você gosta de programar?");
+let respostaA = prompt("Você está usando uma roupa azul hoje?");
+let respostaB = prompt("você tem mais de 20 anos");
+let respostaC = prompt("você gosta de programar?");
+console.log("voce esta usando uma roupa azul hoje?", respostaA);
+console.log("voce tem mais de 20 anos?", respostaB);
+console.log("voce gosta de programar?", respostaC);
+
+//3 inverter valores
+let a = 10
+let b = 25
+let C
+C = a
+a = b
+b = C
+console.log("o valor de a é", a, "o valor de b é", b);
+
+//desafio extra
+prompt("digite um valor");
+prompt("digite outro valor");
+const valor1 = Number(prompt("digite um valor"));
+const valor2 = Number(prompt("digite outro valor"));
+const resultado1 = valor1 + valor2;
+console.log(resultado1);
+const resultado2 = valor1 * valor2;
+console.log(resultado2);
